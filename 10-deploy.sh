a8abb0067dc9cd20a7230109620148b5e13a00e7
Y.K. Goon <ykgoon@gmail.com>
2015-06-15 11:20:07 +0800

    Add Docker-Compose

--- 10-deploy.sh
+++ 10-deploy.sh
@@ -1,6 +1,10 @@
-docker run -d \
-  -v $(pwd)/app:/srv/youtube \
-  -p 80:80 \
-  --name youtube_podcast \
-  youtube_podcast:ubuntu \
-  node server.js
+if ! type docker-compose > /dev/null; then
+    docker run -d \
+	-v $(pwd)/app:/srv/youtube \
+	-p 80:80 \
+	--name youtube_podcast \
+	youtube_podcast:ubuntu \
+	node server.js
+else
+    docker-compose up &
+fi
